62112f2759b320228f6c6efdd77377408fa812b3
jeth318 <jesper.thornberg@me.com>
2019-05-21 14:53:04 +0200

    Add project files.

---
+++ SysAdmin/Controllers/AdminListaController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SysAdmin.Controllers
+{
+    public class AdminListaController : Controller
+    {
+        // GET: AdminLista
+        public ActionResult Index()
+        {
+            System.Diagnostics.Debug.WriteLine("Test:" + Session["anvandarId"]);
+            AdminServerRefferens.InloggningServiceClient klient = new AdminServerRefferens.InloggningServiceClient();
+            List<AdminServerRefferens.Anvandare> lista = new List<AdminServerRefferens.Anvandare>();
+            
+            lista = klient.HamtaAllaAnvandare().ToList();
+
+            int aId = int.Parse(Session["anvandarId"].ToString());
+            if (klient.VerifieraInloggning(aId))
+            {
+                System.Diagnostics.Debug.WriteLine("Vi är inloggad!");
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("Vi är inte inloggad!");
+            }
+
+            return View(lista);
+        }
+        public ActionResult Andra(int? id) // Ändrar värdet i "admin behörighet"
+        {
+            AdminServerRefferens.InloggningServiceClient klient = new AdminServerRefferens.InloggningServiceClient();
+            AdminServerRefferens.Anvandare anvandare = klient.VisaAnvandarInfoId(id.Value);
+            System.Diagnostics.Debug.WriteLine(anvandare.Id + ":" + anvandare.Behorighet);
+            return View(anvandare);
+        }
+        [HttpPost]
+        public ActionResult Andra([Bind(Include = "Id,Behorighet")] AdminServerRefferens.Anvandare anvandare)
+        {
+            System.Diagnostics.Debug.WriteLine("Användare: " + anvandare.Id + " - " + anvandare.Behorighet);
+            AdminServerRefferens.InloggningServiceClient klient = new AdminServerRefferens.InloggningServiceClient();
+            klient.AndraBehorighet(anvandare.Id, anvandare.Behorighet);
+            return RedirectToAction("Index");
+        }
+        public ActionResult TaBort(int? id)
+        {
+            AdminServerRefferens.InloggningServiceClient klient = new AdminServerRefferens.InloggningServiceClient();
+            AdminServerRefferens.Anvandare anvandare = klient.VisaAnvandarInfoId(id.Value);
+
+            return View(anvandare);
+
+        }
+        [HttpPost, ActionName("TaBort")]
+        public ActionResult TaBortAccepterat(int id)
+        {
+
+            AdminServerRefferens.InloggningServiceClient klient = new AdminServerRefferens.InloggningServiceClient();
+            klient.TaBortAnvandare(id);
+            System.Diagnostics.Debug.WriteLine("Ta bort ID: " + id);
+
+            return RedirectToAction("Index");
+        }
+       
+        public ActionResult LaggTillAdmin()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult LaggTillAdmin(string anvandarnamn, string email, string behorighet, string losenord, string losenordRepeat)
+        {
+            if (losenord == losenordRepeat)
+            {
+                AdminServerRefferens.InloggningServiceClient klient = new AdminServerRefferens.InloggningServiceClient();
+                if (klient.VerifieraInloggning(12))
+                {
+                    System.Diagnostics.Debug.WriteLine("Du är inloggad");
+                }
+                else
+                {
+                    System.Diagnostics.Debug.WriteLine("Du är inte inloggad");
+                }
+                klient.RegistreraAdmin(anvandarnamn, losenord, email, behorighet);
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+                ViewBag.VisaFelMeddelande = "De inmatade lösenorden överenstämmer inte med varandra";
+            }
+
+            return View();    
+        }
+    }
+}
---
+++ SysAdmin/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SysAdmin.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string alertMeddelande;
+        // GET: Home
+        public ActionResult Index()
+        {
+            // AdminServerRefferens.InloggningServiceClient kollaOmDuÄrInloggad = new AdminServerRefferens.InloggningServiceClient();
+            //kollaOmDuÄrInloggad.LoggaIn(); 
+            if(Session["LoginError"] != null)
+            {
+                ViewBag.alert = Session["LoginError"];
+            }
+            
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("LoggaIn")]
+        public ActionResult LoggaIn(string anvandarnamn, string losenord)
+        {
+            AdminServerRefferens.InloggningServiceClient kollaOmDuÄrInloggad = new AdminServerRefferens.InloggningServiceClient();
+            AdminServerRefferens.Anvandare anvandare = null;
+            try
+            {
+                anvandare = kollaOmDuÄrInloggad.LoggaIn(anvandarnamn, losenord, "sysadmin");
+
+            }
+
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Error: " + ex.Message);
+
+            }
+
+            if (anvandare != null)
+            {
+                //Inloggningen lyckades
+                Session["anvandarId"] = anvandare.Id;
+                System.Diagnostics.Debug.WriteLine("Loggar in som: " + anvandarnamn);
+                return RedirectToAction("Index", "AdminLista");
+            }
+
+            Session["LoginError"] = "Inloggningen misslyckades, försök igen!";
+            System.Diagnostics.Debug.WriteLine("Inloggningen misslyckades: " + anvandarnamn);
+            return RedirectToAction("Index");
+        }
+        
+    }
+}
